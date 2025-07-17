ec50c9fb2769652118ea1560f2f0be8729c6c67a
phpontes <ppontes95@hotmail.com>
2025-07-17 13:04:05 -0300

    Regras de negócio

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -3,6 +3,7 @@ class Customer {
     _id: string;
     _name: string;
     _address: string;
+    _active: boolean = true;
 
     constructor(id: string, name: string, address: string) {
         this._id = id;
@@ -10,23 +11,15 @@ class Customer {
         this._address = address;
     }
 
-    get id(): string {
-        return this._id;
-    }
-
-    get name(): string {
-        return this._name;
-    }
-
-    get address(): string {
-        return this._address
-    }
-
-    set name(name: string) {
+    changeName(name: string) {
         this._name = name;
     }
 
-    set address(address: string) {
-        this._address = address;
+    activateCustomer() {
+        this._active = true;
+    }
+
+    deactivateCustomer() {
+        this._active = false;
     }
 }
