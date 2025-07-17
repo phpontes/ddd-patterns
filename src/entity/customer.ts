f2b402fcd44ca54d4172f9b6313e72304cabc84a
phpontes <ppontes95@hotmail.com>
2025-07-17 13:23:22 -0300

    Princípio de autovalidação

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -2,20 +2,33 @@ class Customer {
 
     _id: string;
     _name: string;
-    _address: string;
-    _active: boolean = true;
+    _address: string = "";
+    _active: boolean = false;
 
-    constructor(id: string, name: string, address: string) {
+    constructor(id: string, name: string) {
         this._id = id;
         this._name = name;
-        this._address = address;
+        this.validate();
+    }
+
+    validate() {
+        if (this._id.length === 0) {
+            throw new Error("Id is required");
+        }        
+        if (this._name.length === 0) {
+            throw new Error("Name is required");
+        }
     }
 
     changeName(name: string) {
         this._name = name;
+        this.validate();
     }
 
     activateCustomer() {
+        if (this._address.length === 0) {
+            throw new Error("Address is mandatory to activate a customer");
+        }
         this._active = true;
     }
 
