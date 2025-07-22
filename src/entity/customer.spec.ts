976d795035bb11bd2df4b62d44add42e7f4a16bc
phpontes <ppontes95@hotmail.com>
2025-07-22 09:04:54 -0300

    Testando regra de reward points

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -44,4 +44,15 @@ describe("Customer Unit Tests", () => {
 
     expect(customer.isActive()).toBe(false);
   });
+
+  it("should add reward points", () => {
+    const customer = new Customer("1", "Customer 1");
+    expect(customer.rewardPoints).toBe(0);
+
+    customer.addRewardPoints(10);
+    expect(customer.rewardPoints).toBe(10);
+
+    customer.addRewardPoints(10);
+    expect(customer.rewardPoints).toBe(20);
+  });
 });
