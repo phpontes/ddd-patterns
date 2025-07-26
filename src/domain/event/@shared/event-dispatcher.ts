8a9afe3e0caa41c01b353563faf612f0dd79eda3
phpontes <ppontes95@hotmail.com>
2025-07-26 10:13:44 -0300

    Implementando event dispatcher e seus testes

--- src/domain/event/@shared/event-dispatcher.ts
+++ src/domain/event/@shared/event-dispatcher.ts
@@ -1,13 +1,40 @@
 import EventDispatcherInterface from "./event-dispatcher.interface";
 import EventHandlerInterface from "./event-handler.interface";
-import EventInterface from "./event.interface";
+import eventInterface from "./event.interface";
 
 export default class EventDispatcher implements EventDispatcherInterface {
-  notify(event: EventInterface): void {}
+  private eventHandlers: { [eventName: string]: EventHandlerInterface[] } = {};
 
-  register(eventName: string, eventHandler: EventHandlerInterface): void {}
+  get getEventHandlers(): { [eventName: string]: EventHandlerInterface[] } {
+    return this.eventHandlers;
+  }
 
-  unregister(eventName: string, eventHandler: EventHandlerInterface): void {}
+  register(eventName: string, eventHandler: EventHandlerInterface): void {
+    if (!this.eventHandlers[eventName]) {
+      this.eventHandlers[eventName] = [];
+    }
+    this.eventHandlers[eventName].push(eventHandler);
+  }
 
-  unregisterAll(): void {}
+  unregister(eventName: string, eventHandler: EventHandlerInterface): void {
+    if (this.eventHandlers[eventName]) {
+      const index = this.eventHandlers[eventName].indexOf(eventHandler);
+      if (index !== -1) {
+        this.eventHandlers[eventName].splice(index, 1);
+      }
+    }
+  }
+
+  unregisterAll(): void {
+    this.eventHandlers = {};
+  }
+
+  notify(event: eventInterface): void {
+    const eventName = event.constructor.name;
+    if (this.eventHandlers[eventName]) {
+      this.eventHandlers[eventName].forEach((eventHandler) => {
+        eventHandler.handle(event);
+      });
+    }
+  }
 }
